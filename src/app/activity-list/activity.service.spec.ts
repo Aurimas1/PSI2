7a1a88bde6644235051db6ebb9722410d384e17f
Aurimas1 <30459244+Aurimas1@users.noreply.github.com>
2018-05-28 09:37:25 +0300

    One more advanced test

--- src/app/activity-list/activity.service.spec.ts
+++ src/app/activity-list/activity.service.spec.ts
@@ -1,15 +1,49 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ActivityService } from './activity.service';
+import { Mock } from 'ts-mocks';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AuthService } from '../login/auth.service';
+import { ToastsManager } from 'ng2-toastr';
+import { Activity } from '../models/activity';
 
-describe('ActivityService', () => {
+fdescribe('ActivityService', () => {
+
+  let err: jasmine.Spy;
+
+  const fakeActivity: Activity = {
+    description: '_description',
+    start: undefined,
+    end: undefined,
+    name: '_name',
+    users: ['fakeUser']
+  };
+
   beforeEach(() => {
+
+    const afs = new Mock<AngularFirestore>();
+
+    const auth = new Mock<AuthService>();
+    auth.setup(x => x.username).is('fakeUser');
+
+    const toast = new Mock<ToastsManager>();
+    toast.setup(x => x.error).is(x => Promise.resolve<any>({}));
+    err = toast.spyOf(x => x.error);
+
     TestBed.configureTestingModule({
-      providers: [ActivityService]
+      providers: [ActivityService,
+                  {provide: AngularFirestore, useValue: afs.Object},
+                  {provide: AuthService, useValue: auth.Object},
+                  {provide: ToastsManager, useValue: toast.Object}]
     });
   });
 
   it('should be created', inject([ActivityService], (service: ActivityService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('joinActivity should show error if you allready in it', inject([ActivityService], (service: ActivityService) => {
+    service.joinActivity(fakeActivity);
+    expect(err).toHaveBeenCalledTimes(1);
   }));
 });
--- src/app/login/auth.service.spec.ts
+++ src/app/login/auth.service.spec.ts
@@ -1,11 +1,26 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
+import { Mock } from 'ts-mocks';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { User } from '@firebase/auth-types';
 
-describe('AuthService', () => {
+fdescribe('AuthService', () => {
   beforeEach(() => {
+
+    const authState = new BehaviorSubject<User>({} as User);
+
+    const auth = new Mock<AngularFireAuth>();
+    auth.setup(x => x.authState).is(authState.asObservable());
+
+    const router = new Mock<Router>();
+
     TestBed.configureTestingModule({
-      providers: [AuthService]
+      providers: [AuthService,
+                 {provide: AngularFireAuth, useValue: auth.Object},
+                 {provide: Router, useValue: router.Object}]
     });
   });
 
